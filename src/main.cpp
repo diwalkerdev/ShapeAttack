118bfa5fb722488cf00b44f8ab7bc80fe7de2cd1
David Walker <dwalker0044@gmail.com>
2017-08-06 22:16:52 +0100

    fire bullets

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <complex>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include "screen.h"
 
@@ -20,12 +21,16 @@ struct GameRect {
   SDL_Rect rect;
 };
 
+auto game_rects = std::vector<GameRect>(5);
+auto bullets = std::vector<GameRect>(5);
+auto bullet_visibility = std::vector<int>(5);
+
 bool is_collision(SDL_Rect& rect1, SDL_Rect& rect2) {
   return (rect1.x < rect2.x + rect2.w && rect1.x + rect1.w > rect2.x &&
           rect1.y < rect2.y + rect2.h && rect1.h + rect1.y > rect2.y);
 }
 
-void init_game_rect(GameRect& rect) {
+void init_attacking_shapes(GameRect& rect) {
   auto angle = (rand() % 10) *
                36.0;  // 10 starting points about the circle seems enough.
   auto theta = angle * radians;
@@ -47,6 +52,38 @@ void init_game_rect(GameRect& rect) {
   rect.rect.h = 32;
   rect.rect.x = x;
   rect.rect.y = y;
+}
+
+void init_bullets(GameRect& rect, int origin_x, int origin_y) {
+  rect.x = origin_x;
+  rect.y = origin_y;
+  rect.theta = 0;
+  rect.rect.w = 16;
+  rect.rect.h = 16;
+  rect.rect.x = origin_x;
+  rect.rect.y = origin_y;
+}
+
+void handle_player_input() {
+  SDL_Event event;
+
+  SDL_PollEvent(&event);
+  switch (event.type) {
+    case SDL_KEYDOWN: {
+      std::cout << "Got keydown event." << std::endl;
+      if (event.key.keysym.sym == SDLK_SPACE) {
+        std::cout << "Fire!!! Peow Peow!\n" << std::endl;
+        auto it = std::find(
+            std::begin(bullet_visibility), std::end(bullet_visibility), 0);
+        if (it == std::end(bullet_visibility)) {
+          printf("No more bullets");
+        } else {
+          // The bullet is now visible.
+          *it = 1;
+        }
+      }
+    }
+  }
 }
 
 int main() {
@@ -80,9 +117,16 @@ int main() {
   // Init game objects
   auto background_rect = SDL_Rect{0, 0, screen.width, screen.height};
 
-  auto game_rects = std::vector<GameRect>(5);
   for (auto& r : game_rects) {
-    init_game_rect(r);
+    init_attacking_shapes(r);
+  }
+
+  for (auto& v : bullet_visibility) {
+    v = 0;
+  }
+
+  for (auto& b : bullets) {
+    init_bullets(b, screen.center.x - 8, screen.center.y - 8);
   }
 
   int x = screen.center.x - 16;
@@ -90,6 +134,9 @@ int main() {
   auto player = SDL_Rect{x, y, 32, 32};
 
   for (int i = 0; i < 300; i++) {
+    handle_player_input();
+    fflush(stdout);
+
     // Clear the screen
     SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderFillRect(renderer, &background_rect);
@@ -104,15 +151,35 @@ int main() {
       const auto dy = 1.0 * sin(rect.theta);
       rect.x += dx;
       rect.y += dy;
-      rect.rect.x = rect.x;
-      rect.rect.y = rect.y;
+      rect.rect.x = static_cast<int>(rect.x);
+      rect.rect.y = static_cast<int>(rect.y);
 
-      if (is_collision(rect.rect, player)) {
-        std::cout << "!!!" << std::endl;
-      }
+      // if (is_collision(rect.rect, player)) {
+      //   std::cout << "!!!" << std::endl;
+      // }
 
       SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
       SDL_RenderFillRect(renderer, &rect.rect);
+    }
+
+    for (int v = 0; v < 5; ++v) {
+      if (bullet_visibility[v] == 1) {
+        auto& b = bullets[v];
+
+        const auto dx = 1.0 * cos(b.theta);
+        const auto dy = 1.0 * sin(b.theta);
+        b.x += dx;
+        b.y += dy;
+        b.rect.x = static_cast<int>(b.x);
+        b.rect.y = static_cast<int>(b.y);
+
+        // if (is_collision(rect.rect, player)) {
+        //   std::cout << "!!!" << std::endl;
+        // }
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_RenderFillRect(renderer, &b.rect);
+      }
     }
 
     // Update the screen with rendering actions
